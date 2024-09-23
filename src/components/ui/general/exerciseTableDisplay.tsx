0f858fc272187a69f724607f9487741f7799b5bd
scooterh4 <scott.holmes44@gmail.com>
2024-09-23 14:41:56 -0700

    Can add/remove last set of an exercise

--- src/components/ui/general/exerciseTableDisplay.tsx
+++ src/components/ui/general/exerciseTableDisplay.tsx
@@ -1,7 +1,28 @@
 import { type Exercises } from "@prisma/client";
-import { TableBody, TableCell, TableHead, TableHeader, TableRow } from "../table";
+import { TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "../table";
 import { Input } from "../input";
 import { api } from "~/utils/api";
+import { useEffect, useState } from "react";
+import { Button } from "../button";
+import { Trash2Icon } from "lucide-react";
+
+type DisplaySets = {
+  exercise_id: string,
+  prev_set: string,
+  set_num: number,
+  weight: number | null,
+  reps: number | null,
+}
+
+const emptySet = (exerciseId: string, setNum: number) => {
+  return {
+    exercise_id: exerciseId,
+    prev_set: "",
+    set_num: setNum,
+    weight: null,
+    reps: null
+  }
+}
 
 export const ExerciseTableDisplay = (
   {exercise}: {exercise: Exercises}
@@ -9,29 +30,46 @@ export const ExerciseTableDisplay = (
   const prevSets = api.exercise.getPreviousSetsForExercise.useQuery({
     exerciseId: exercise.id
   }).data
-  
-  let displaySets
-  if (!prevSets) {
-    displaySets = [1, 2, 3].map(num => {
-      return {
-        exercise_id: exercise.id,
-        prev_set: "",
-        set_num: num,
-        weight: null,
-        reps: null
-      }
-    })
-  } 
-  else {
-    displaySets = prevSets.map(set => {
-      return {
-        exercise_id: set.exercise_id,
-        prev_set: `${set.weight} x ${set.reps}`,
-        set_num: set.set_num,
-        weight: null,
-        reps: null
-      } 
-    })
+
+  useEffect(() => {
+    if (!prevSets) {
+      setDisplaySets([1, 2, 3].map(num => {
+        return emptySet(exercise.id, num);
+      }))
+    }
+    else {
+      setDisplaySets(prevSets.map(set => {
+        return {
+          exercise_id: set.exercise_id,
+          prev_set: `${set.weight} x ${set.reps}`,
+          set_num: set.set_num,
+          weight: null,
+          reps: null
+        };
+      }))
+    }
+  }, [prevSets])
+
+  const [ displaySets, setDisplaySets ] = useState<DisplaySets[]>([])
+
+  const addSetToExercise = () => {
+    const lastSet = displaySets[displaySets.length - 1]?.set_num
+    if (!lastSet || lastSet === undefined) {
+      console.log("Last set is undefined for exercise id", exercise.id)
+    }
+    else {
+      setDisplaySets((prevSets) => [...prevSets, emptySet(exercise.id, lastSet + 1)])
+    }
+  }
+
+  const deleteSet = (setNum: number) => {
+    if (setNum === displaySets.length) {
+      setDisplaySets((prevSets) => {
+        const sets = [...prevSets]
+        sets.pop()
+        return sets
+      })
+    }
   }
 
   return (
@@ -39,7 +77,7 @@ export const ExerciseTableDisplay = (
       <TableHeader className="text-right">{exercise.exercise_name}</TableHeader> 
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Set</TableHead>
+          <TableHead className="w-[30px]">Set</TableHead>
           <TableHead>Previous display</TableHead>
           <TableHead>Weight</TableHead>
           <TableHead>Reps</TableHead>
@@ -50,29 +88,19 @@ export const ExerciseTableDisplay = (
           <TableRow key={set.set_num}>
             <TableCell className="font-medium">{set.set_num}</TableCell>
             <TableCell>{set.prev_set}</TableCell>
-            <TableCell><Input /></TableCell>
-            <TableCell className="text-right"><Input /></TableCell>
+            <TableCell><Input type="number" /></TableCell>
+            <TableCell className="text-right"><Input type="number" /></TableCell>
+            <TableCell onClick={() => deleteSet(set.set_num)}>
+              <Trash2Icon className={`${set.set_num !== displaySets.length ? "text-gray-300" : ""}`} />
+            </TableCell>
           </TableRow>
         ))}
-          {/* <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>165 x 5</TableCell>
-            <TableCell><Input /></TableCell>
-            <TableCell className="text-right"><Input /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">2</TableCell>
-            <TableCell>170 x 4</TableCell>
-            <TableCell><Input /></TableCell>
-            <TableCell className="text-right"><Input /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">3</TableCell>
-            <TableCell>175 x 3</TableCell>
-            <TableCell><Input /></TableCell>
-            <TableCell className="text-right"><Input /></TableCell>
-          </TableRow> */}
       </TableBody>
+      <TableFooter className="w-full">
+        <Button className="w-full" onClick={addSetToExercise}>
+          Add set
+        </Button>
+      </TableFooter>
     </>
   )
 }
